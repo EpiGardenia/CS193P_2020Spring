123a213cb472811868e9c2c3606831d560a727f3
AlaacLux <AlaacLux@outlook.com>
2021-03-12 17:15:30 +0100

    Lecture 6 : Pie animation

---
+++ Projects/Lectures/Lecture6/Memorize/Memorize/Grid.swift
@@ -0,0 +1,43 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by T  on 2021-03-11.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
+    private var items: [Item]
+    private var viewForItem: (Item) -> ItemView
+
+    // @escaping: won't be init until needed.
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
+    }
+
+    private func body(for layout: GridLayout) -> some View {
+        // ForEach is also @escaping
+        ForEach(items) { item in
+            self.body(for: item, in: layout)
+        }
+    }
+
+    private func body(for item: Item, in layout: GridLayout) -> some View {
+        let index = items.firstIndex(matching: item)
+        return Group {
+            if index != nil {
+                viewForItem(item)
+                    .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+                    .position(layout.location(ofItemAt: index!))
+            }
+        }
+    }
+}
